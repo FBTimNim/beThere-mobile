7e8a7b9bb592208371f7eb273837bec01f198f40
Thomas Wang <dev@paced.me>
2017-05-07 08:01:20 +1000

    Styling more or less finished

--- BeThereMobile/www/scripts/loadMap.js
+++ BeThereMobile/www/scripts/loadMap.js
@@ -1,221 +1,155 @@
 var map;
 var markerList = [];
 
-var iconURLs = ['images/listen.png', 'images/view.png', 'images/watch.png'];
+var iconURLs = [ 'images/listen.png', 'images/view.png', 'images/watch.png' ];
 var namesList = [];
 
 function initMap() {
   // Melbourne CBD
-  var startingLocation = {
-    lat: -37.813397,
-    lng: 144.965072
-  };
+  var startingLocation = {lat : -37.813397, lng : 144.965072};
   map = new google.maps.Map(document.getElementById('map'), {
-    center: startingLocation,
-    zoom: 13,
-    streetViewControl: false,
-    disableDefaultUI: true,
-    styles: [{
-        "elementType": "geometry",
-        "stylers": [{
-          "color": "#ebe3cd"
-        }]
+    center : startingLocation,
+    zoom : 13,
+    streetViewControl : false,
+    disableDefaultUI : true,
+    styles : [
+      {"elementType" : "geometry", "stylers" : [ {"color" : "#ebe3cd"} ]},
+      {
+        "elementType" : "labels.text.fill",
+        "stylers" : [ {"color" : "#523735"} ]
       },
       {
-        "elementType": "labels.text.fill",
-        "stylers": [{
-          "color": "#523735"
-        }]
+        "elementType" : "labels.text.stroke",
+        "stylers" : [ {"color" : "#f5f1e6"} ]
       },
       {
-        "elementType": "labels.text.stroke",
-        "stylers": [{
-          "color": "#f5f1e6"
-        }]
+        "featureType" : "administrative",
+        "elementType" : "geometry",
+        "stylers" : [ {"visibility" : "off"} ]
       },
       {
-        "featureType": "administrative",
-        "elementType": "geometry",
-        "stylers": [{
-          "visibility": "off"
-        }]
+        "featureType" : "administrative",
+        "elementType" : "geometry.stroke",
+        "stylers" : [ {"color" : "#c9b2a6"} ]
       },
       {
-        "featureType": "administrative",
-        "elementType": "geometry.stroke",
-        "stylers": [{
-          "color": "#c9b2a6"
-        }]
+        "featureType" : "administrative.land_parcel",
+        "elementType" : "geometry.stroke",
+        "stylers" : [ {"color" : "#dcd2be"} ]
       },
       {
-        "featureType": "administrative.land_parcel",
-        "elementType": "geometry.stroke",
-        "stylers": [{
-          "color": "#dcd2be"
-        }]
+        "featureType" : "administrative.land_parcel",
+        "elementType" : "labels.text.fill",
+        "stylers" : [ {"color" : "#ae9e90"} ]
       },
       {
-        "featureType": "administrative.land_parcel",
-        "elementType": "labels.text.fill",
-        "stylers": [{
-          "color": "#ae9e90"
-        }]
+        "featureType" : "landscape.natural",
+        "elementType" : "geometry",
+        "stylers" : [ {"color" : "#dfd2ae"} ]
       },
       {
-        "featureType": "landscape.natural",
-        "elementType": "geometry",
-        "stylers": [{
-          "color": "#dfd2ae"
-        }]
+        "featureType" : "poi",
+        "elementType" : "geometry",
+        "stylers" : [ {"color" : "#dfd2ae"} ]
       },
       {
-        "featureType": "poi",
-        "elementType": "geometry",
-        "stylers": [{
-          "color": "#dfd2ae"
-        }]
+        "featureType" : "poi",
+        "elementType" : "labels",
+        "stylers" : [ {"visibility" : "off"} ]
       },
       {
-        "featureType": "poi",
-        "elementType": "labels",
-        "stylers": [{
-          "visibility": "off"
-        }]
+        "featureType" : "poi",
+        "elementType" : "labels.icon",
+        "stylers" : [ {"visibility" : "off"} ]
       },
       {
-        "featureType": "poi",
-        "elementType": "labels.icon",
-        "stylers": [{
-          "visibility": "off"
-        }]
+        "featureType" : "poi",
+        "elementType" : "labels.text.fill",
+        "stylers" : [ {"color" : "#93817c"} ]
       },
       {
-        "featureType": "poi",
-        "elementType": "labels.text.fill",
-        "stylers": [{
-          "color": "#93817c"
-        }]
+        "featureType" : "poi.park",
+        "elementType" : "geometry.fill",
+        "stylers" : [ {"color" : "#a5b076"} ]
       },
       {
-        "featureType": "poi.park",
-        "elementType": "geometry.fill",
-        "stylers": [{
-          "color": "#a5b076"
-        }]
+        "featureType" : "poi.park",
+        "elementType" : "labels.text.fill",
+        "stylers" : [ {"color" : "#447530"} ]
       },
       {
-        "featureType": "poi.park",
-        "elementType": "labels.text.fill",
-        "stylers": [{
-          "color": "#447530"
-        }]
+        "featureType" : "road",
+        "elementType" : "geometry",
+        "stylers" : [ {"color" : "#f5f1e6"} ]
       },
       {
-        "featureType": "road",
-        "elementType": "geometry",
-        "stylers": [{
-          "color": "#f5f1e6"
-        }]
+        "featureType" : "road",
+        "elementType" : "labels.icon",
+        "stylers" : [ {"visibility" : "off"} ]
       },
       {
-        "featureType": "road",
-        "elementType": "labels.icon",
-        "stylers": [{
-          "visibility": "off"
-        }]
+        "featureType" : "road.arterial",
+        "elementType" : "geometry",
+        "stylers" : [ {"color" : "#fdfcf8"} ]
       },
       {
-        "featureType": "road.arterial",
-        "elementType": "geometry",
-        "stylers": [{
-          "color": "#fdfcf8"
-        }]
+        "featureType" : "road.highway",
+        "elementType" : "geometry",
+        "stylers" : [ {"color" : "#f8c967"} ]
       },
       {
-        "featureType": "road.highway",
-        "elementType": "geometry",
-        "stylers": [{
-          "color": "#f8c967"
-        }]
+        "featureType" : "road.highway",
+        "elementType" : "geometry.stroke",
+        "stylers" : [ {"color" : "#e9bc62"} ]
       },
       {
-        "featureType": "road.highway",
-        "elementType": "geometry.stroke",
-        "stylers": [{
-          "color": "#e9bc62"
-        }]
+        "featureType" : "road.highway.controlled_access",
+        "elementType" : "geometry",
+        "stylers" : [ {"color" : "#e98d58"} ]
       },
       {
-        "featureType": "road.highway.controlled_access",
-        "elementType": "geometry",
-        "stylers": [{
-          "color": "#e98d58"
-        }]
+        "featureType" : "road.highway.controlled_access",
+        "elementType" : "geometry.stroke",
+        "stylers" : [ {"color" : "#db8555"} ]
       },
       {
-        "featureType": "road.highway.controlled_access",
-        "elementType": "geometry.stroke",
-        "stylers": [{
-          "color": "#db8555"
-        }]
+        "featureType" : "road.local",
+        "elementType" : "labels.text.fill",
+        "stylers" : [ {"color" : "#806b63"} ]
+      },
+      {"featureType" : "transit", "stylers" : [ {"visibility" : "off"} ]},
+      {
+        "featureType" : "transit.line",
+        "elementType" : "geometry",
+        "stylers" : [ {"color" : "#dfd2ae"} ]
       },
       {
-        "featureType": "road.local",
-        "elementType": "labels.text.fill",
-        "stylers": [{
-          "color": "#806b63"
-        }]
+        "featureType" : "transit.line",
+        "elementType" : "labels.text.fill",
+        "stylers" : [ {"color" : "#8f7d77"} ]
       },
       {
-        "featureType": "transit",
-        "stylers": [{
-          "visibility": "off"
-        }]
+        "featureType" : "transit.line",
+        "elementType" : "labels.text.stroke",
+        "stylers" : [ {"color" : "#ebe3cd"} ]
       },
       {
-        "featureType": "transit.line",
-        "elementType": "geometry",
-        "stylers": [{
-          "color": "#dfd2ae"
-        }]
+        "featureType" : "transit.station",
+        "elementType" : "geometry",
+        "stylers" : [ {"color" : "#dfd2ae"} ]
       },
       {
-        "featureType": "transit.line",
-        "elementType": "labels.text.fill",
-        "stylers": [{
-          "color": "#8f7d77"
-        }]
+        "featureType" : "water",
+        "elementType" : "geometry.fill",
+        "stylers" : [ {"color" : "#b9d3c2"} ]
       },
       {
-        "featureType": "transit.line",
-        "elementType": "labels.text.stroke",
-        "stylers": [{
-          "color": "#ebe3cd"
-        }]
-      },
-      {
-        "featureType": "transit.station",
-        "elementType": "geometry",
-        "stylers": [{
-          "color": "#dfd2ae"
-        }]
-      },
-      {
-        "featureType": "water",
-        "elementType": "geometry.fill",
-        "stylers": [{
-          "color": "#b9d3c2"
-        }]
-      },
-      {
-        "featureType": "water",
-        "elementType": "labels.text.fill",
-        "stylers": [{
-          "color": "#92998d"
-        }]
+        "featureType" : "water",
+        "elementType" : "labels.text.fill",
+        "stylers" : [ {"color" : "#92998d"} ]
       }
     ],
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    mapTypeId : google.maps.MapTypeId.ROADMAP
   });
 
   makeApiRequestForMarkers();
@@ -225,39 +159,25 @@ function initMap() {
 }
 
 function changeMapLocation(newLoc) {
-  map.center = {
-    lat: newLoc.coords.latitude,
-    lng: newLoc.coords.longitude
-  };
+  map.center = {lat : newLoc.coords.latitude, lng : newLoc.coords.longitude};
 }
 
 function addEventCircles() {
-  var eventLocations = [{
-      center: {
-        lat: -37.813628,
-        lng: 144.963058
-      },
-      radius: 3000
-    },
-    {
-      center: {
-        lat: -37.864000,
-        lng: 144.982000
-      },
-      radius: 2000
-    }
+  var eventLocations = [
+    {center : {lat : -37.813628, lng : 144.963058}, radius : 3000},
+    {center : {lat : -37.864000, lng : 144.982000}, radius : 2000}
   ];
   for (var event in eventLocations) {
     // Add the circle for this city to the map.
     var eventRadius = new google.maps.Circle({
-      strokeColor: '#FF0000',
-      strokeOpacity: 0,
-      strokeWeight: 1,
-      fillColor: '#b5c3fc',
-      fillOpacity: 0.25,
-      map: map,
-      center: eventLocations[event].center,
-      radius: eventLocations[event].radius
+      strokeColor : '#FF0000',
+      strokeOpacity : 0,
+      strokeWeight : 1,
+      fillColor : '#b5c3fc',
+      fillOpacity : 0.25,
+      map : map,
+      center : eventLocations[event].center,
+      radius : eventLocations[event].radius
     });
   }
 }
@@ -299,46 +219,38 @@ function getNameHtml(names) {
 function makeApiRequestForMarkers() {
   console.log("making request");
   $.post("http://108.61.194.210/api/getRelevant", {
-    "apikey": "pekdaYjYqAPkAmjT0953s4U2Z3jaW04bz0uAUfdZ36RfMdCnkF0Bf0Odcptx9A3j",
-    "withinTime": 1,
-  }).done(function(data) {
-    addMapMarkers(data.data);
-  });
+     "apikey" :
+         "pekdaYjYqAPkAmjT0953s4U2Z3jaW04bz0uAUfdZ36RfMdCnkF0Bf0Odcptx9A3j",
+     "withinTime" : 1,
+   }).done(function(data) { addMapMarkers(data.data); });
 }
 
 function addMapMarkers(data) {
   // Add Markers
   console.log(data);
 
-  var mediaList = [{
-      position: {
-        lat: -37.806548,
-        lng: 144.971080
-      },
-      mediaType: 'photo',
-      mediaID: 'https://farm3.staticflickr.com/2808/34282062515_6192a67cfd.jpg',
-      name: 'Fred Smith',
-      thumbURL: 'images/listen.png'
+  var mediaList = [
+    {
+      position : {lat : -37.806548, lng : 144.971080},
+      mediaType : 'photo',
+      mediaID :
+          'https://farm3.staticflickr.com/2808/34282062515_6192a67cfd.jpg',
+      name : 'Fred Smith',
+      thumbURL : 'images/listen.png'
     },
     {
-      position: {
-        lat: -37.810142,
-        lng: 144.958377
-      },
-      mediaType: 'video',
-      mediaID: 'YJEjQglAdOk',
-      thumbURL: 'images/listen.png',
-      name: 'Fred Jones',
+      position : {lat : -37.810142, lng : 144.958377},
+      mediaType : 'video',
+      mediaID : 'YJEjQglAdOk',
+      thumbURL : 'images/listen.png',
+      name : 'Fred Jones',
     },
     {
-      position: {
-        lat: -37.816371,
-        lng: 144.962647
-      },
-      mediaType: 'audio',
-      mediaID: '3',
-      thumbURL: 'images/listen.png',
-      name: 'Fred Jenkins',
+      position : {lat : -37.816371, lng : 144.962647},
+      mediaType : 'audio',
+      mediaID : '3',
+      thumbURL : 'images/listen.png',
+      name : 'Fred Jenkins',
     }
   ];
 
@@ -346,10 +258,20 @@ function addMapMarkers(data) {
     markerList.push(createMarker(data[i]));
   }
 
-  var markerCluster =
-    new MarkerClusterer(map, markerList, {
-      imagePath: 'images/m'
-    });
+  var mcOptions = {
+    imagePath : 'https://googlemaps.github.io/js-marker-clusterer/images/m',
+    styles : [ {
+
+      url : "https://googlemaps.github.io/js-marker-clusterer/images/m2.png",
+      width : 53,
+      height : 53,
+      textSize : 12,
+      textColor : "white"
+    } ]
+
+  };
+
+  var markerCluster = new MarkerClusterer(map, markerList, mcOptions);
 }
 
 function createMarker(mediaItem) {
@@ -358,13 +280,11 @@ function createMarker(mediaItem) {
   var iconURL = 'http://108.61.194.210/api' + mediaItem.thumbUrl;
 
   var marker = new google.maps.Marker({
-    title: mediaItem.name,
-    position: {
-      lat: parseFloat(mediaItem.lat),
-      lng: parseFloat(mediaItem.lon)
-    },
-    map: map,
-    icon: iconURL,
+    title : mediaItem.name,
+    position :
+        {lat : parseFloat(mediaItem.lat), lng : parseFloat(mediaItem.lon)},
+    map : map,
+    icon : iconURL,
   });
   marker.name = mediaItem.name;
   marker.mediaType = mediaItem.type;
@@ -377,8 +297,8 @@ function createMarker(mediaItem) {
 
 function showHostedVideo() {
   var html = '<video width="320" height="240" controls>' +
-    '<source src="' + this.url + '" type="video/mp4">' +
-    "</video>";
+             '<source src="' + this.url + '" type="video/mp4">' +
+             "</video>";
 
   emptyMediaDev();
   $(".media").append(html);
@@ -390,8 +310,8 @@ function showVideo() {
   emptyMediaDev();
 
   $(".media").append(
-    '<div class = "videoContainer"> <iframe src="http://www.youtube.com/embed/' +
-    mediaID + '" width="560" height="315" frameborder="0" ></iframe> </div>');
+      '<div class = "videoContainer"> <iframe src="http://www.youtube.com/embed/' +
+      mediaID + '" width="560" height="315" frameborder="0" ></iframe> </div>');
 }
 
 function showImage() {
@@ -400,14 +320,12 @@ function showImage() {
   emptyMediaDev();
 
   $(".media").append(
-    '<div class="content-block-title">Media</div>' +
-    '<div class="col-100" id="imgViewer"> <img id="img-media" src="' +
-    mediaID + '" /></div>');
+      '<div class="content-block-title">Media</div>' +
+      '<div class="col-100" id="imgViewer"> <img id="img-media" src="' +
+      mediaID + '" /></div>');
 }
 
-function showAudio() {
-  alert("Media: " + this.mediaType + " " + this.mediaID);
-}
+function showAudio() { alert("Media: " + this.mediaType + " " + this.mediaID); }
 
 function getFunctionForMediaType(mediaType) {
   if (mediaType == 'video') {
@@ -417,9 +335,7 @@ function getFunctionForMediaType(mediaType) {
   }
 }
 
-function emptyMediaDev() {
-  $(".media").empty();
-}
+function emptyMediaDev() { $(".media").empty(); }
 
 function getIconURL(mediaType) {
   if (mediaType == 'photo') {
@@ -434,7 +350,7 @@ function getIconURL(mediaType) {
 
 function displayImage(mediaObject) {
   $('img-media')
-    .src('https://farm3.staticflickr.com/2808/34282062515_6192a67cfd.jpg');
+      .src('https://farm3.staticflickr.com/2808/34282062515_6192a67cfd.jpg');
 
   $('img-viewer').show();
 }
