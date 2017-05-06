0d6233138c89be5494095942d2d02d3f7ca651f8
Radium Zheng <ztl8702@msn.com>
2017-05-07 02:15:49 +1000

    Facebook login (for real)

--- BeThereMobile/www/scripts/index.js
+++ BeThereMobile/www/scripts/index.js
@@ -10,6 +10,10 @@
     var SERVER_URL = "http://what";
     var API_KEY = "pekdaYjYqAPkAmjT0953s4U2Z3jaW04bz0uAUfdZ36RfMdCnkF0Bf0Odcptx9A3j";
 
+    window.FBLogin = {
+        loggedIn : false
+    };
+
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
         document.addEventListener( 'pause', onPause.bind( this ), false );
@@ -19,20 +23,51 @@
 
         console.log("Device is Ready");
 
+        // Export selectors engine
+        window.$$ = Dom7;
+
 
         // Initialize your app
         window.myApp = new Framework7({
-            material: true
+            material: true,
+            init: false
         });
 
-        // Export selectors engine
-        window.$$ = Dom7;
+
+        myApp.onPageInit('index', function (page) {
+            console.log('onload');
+            $$(page.container).find("#btnfblogin").on("click", function (e) {
+                console.log('begin FB login');
+                var fbLoginSuccess = function (userData) {
+                    window.FBLogin['loggedIn'] = true;
+                    window.FBLogin['userId'] = userData['authResponse']['userID'];
+                    console.log(userData);
+                    //alert("UserInfo: " + JSON.stringify(userData));
+                    alert('Logged in!');
+                    facebookConnectPlugin.getAccessToken(function (token) {
+                        //alert("Token: " + token);
+                        window.FBLogin['accessToken'] = token;
+                    }, function (err) {
+                        alert("Could not get access token: " + err);
+                        console.log(err);
+                    });
+                }
+
+                facebookConnectPlugin.login(["public_profile"],
+                    fbLoginSuccess,                                   
+                    function (error) { alert("error" + error); console.log(error); }
+                );
+            });
+        });
+
 
         // Add view
         window.mainView = myApp.addView('.view-main', {
             // Because we use fixed-through navbar we can enable dynamic navbar
             dynamicNavbar: true
         });
+
+
 
         /** Photo Upload Page **/
         myApp.onPageInit('uploadphoto', function (page) {
@@ -154,6 +189,8 @@
 
             });
         });
+
+        myApp.init();
     };
 
     function onPause() {
