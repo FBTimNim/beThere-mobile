c054799f0e3cffc04147fd4ffba2aa1e083fcd6b
Thomas Wang <dev@paced.me>
2017-05-09 18:09:09 +1000

    Welcome screen will now only show for new users

--- BeThereMobile/www/scripts/calSetup.js
+++
@@ -1,7 +0,0 @@
-var myApp = new Framework7();
-
-var calendarRange = myApp.calendar({
-    input: '#calendar-range',
-    dateFormat: 'M dd yyyy',
-    rangePicker: true,
-});
---
+++ BeThereMobile/www/scripts/setup.js
@@ -0,0 +1,1 @@
+var myapp = new Framework7();
